4725b1482fe2ece1c8344a12d303a6685f99f9b4
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-04-19 21:25:33 +0530

    Github api handled successfully

--- src/Components/GitHub/Github.jsx
+++ src/Components/GitHub/Github.jsx
@@ -1,21 +1,42 @@
 import React, { useEffect, useState } from "react";
-
+import { useParams } from "react-router-dom";
 function Github() {
   const [data, setData] = useState({});
+  const id = useParams()
   useEffect(() => {
-    fetch("https://api.github.com/users/CodersHub-ing")
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-      });
-  }, [{}]);
+    if (id) {
+      fetch("https://api.github.com/users/CodersHub-in")
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+          console.log("Fetched");
+        });
+
+    }
+  }, []);
   return (
-    <div>
+    <div className="bg-black h-[100vh] text-white " >
+      <div className="flex justify-center text-center  ">
+        <h1 className="text-3xl text-center  " >GitHub profile </h1>
+      </div>
       <div>
-        <h1  className="text-3xl text-center  " >GitHub profile </h1>
+        <div className=" flex justify-center text-center w-min-[10vw]  w-max  items-center p-2 border-2 border-solid border-white rounded-xl bg-[#212121] transition-all shadow-inner shadow-white" >
+          
+          
+          <div   className="m-2">
+            <img src={data.avatar_url ? data.avatar_url : "https://avatars.githubusercontent.com/u/154464457?v=4"} className="h-60 w-60 rounded-[50%] border-2 border-solid border-gray-500" alt="" />
+            <h1 className="text-xl"  >{(data.login ? data.login : "gitHub Login error please login again  |or| wait a minute")}</h1>
+          </div>
+           
+          
+
+          <div>
+            <h1>{"Followers are " + data.followers}</h1>
+          </div>
+        </div>
       </div>
 
-      {data.login}
+
     </div>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <header className='sticky top-0 bg-black'>
       <div className='flex    p-1 rounded-lg bg-[#212121] justify-between'>
         <div className='flex border-solid border-2 border-gray-700  p-1 rounded-2xl gap-1 '>
-          <img src="src\Components\Header\resources\RoBoico.png" className='h-10 w-10 rounded-[50%]' alt="" />
+          <img src="https://raw.githubusercontent.com/CodersHub-in/Portfolio/main/src/Components/Header/resources/RoBoico.png" className='h-10 w-10 rounded-[50%]' alt="" />
           <h2 className='text-3xl text-white'>Basic Router</h2>
         </div>
         <div className='flex gap-1 justify-center text-center items-center p-1 '>
@@ -21,7 +21,7 @@ function Header() {
             Home
           </NavLink>
           <NavLink
-            to="/github"
+            to="/github/CodersHub-in"
             className={({ isActive }) =>
               ` flex justify-center text-center items-center  py-2 pr-4 pl-3 duration-200  ${isActive ? "text-orange-700 underline" : "text-white"} 
             bg-gray-500 w-20 h-8 hover:shadow-cyan-500/50 rounded-[30px]  cursor-pointer border-2 border-black border-solid border-b  hover:bg-black   lg:border-0 hover:text-orange-700 lg:p-0`
